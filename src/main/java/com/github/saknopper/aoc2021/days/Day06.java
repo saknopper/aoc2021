bfc8e56b3949a1748da359f1d7d900d148d064b0
Sander Knopper <s.knopper@flusso.nl>
2021-12-06 17:41:40 +0100

    Day 6 - use byte to save some memory, refactor input parsing

--- src/main/java/com/github/saknopper/aoc2021/days/Day06.java
+++ src/main/java/com/github/saknopper/aoc2021/days/Day06.java
@@ -1,5 +1,7 @@
 package com.github.saknopper.aoc2021.days;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,20 +9,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Day06 extends Day
 {
     @Override
     public String getAnswerPartOne() throws Exception {
-        Path path = Paths.get(getClass().getClassLoader().getResource("day06.txt").toURI());
-        List<String> lines = Files.readAllLines(path);
-        List<Integer> fish = Arrays.stream(lines.get(0).split(",")).map(Integer::valueOf).toList();
-        Map<Integer, Long> fishCountPerTimer = new HashMap<>();
-        for (var f : fish)
-            if (fishCountPerTimer.computeIfPresent(f, (timer, count) -> count + 1) == null)
-                fishCountPerTimer.put(f, 1l);
-
+        Map<Byte, Long> fishCountPerTimer = parseInput();
         fishCountPerTimer = fishSimulation(fishCountPerTimer, 80);
 
         return String.valueOf(fishCountPerTimer.values().stream().mapToLong(Long::longValue).sum());
@@ -28,30 +22,35 @@ public class Day06 extends Day
 
     @Override
     public String getAnswerPartTwo() throws Exception {
-        Path path = Paths.get(getClass().getClassLoader().getResource("day06.txt").toURI());
-        List<String> lines = Files.readAllLines(path);
-        List<Integer> fish = Arrays.stream(lines.get(0).split(",")).map(Integer::valueOf).toList();
-        Map<Integer, Long> fishCountPerTimer = new HashMap<>();
-        for (var f : fish)
-            if (fishCountPerTimer.computeIfPresent(f, (timer, count) -> count + 1) == null)
-                fishCountPerTimer.put(f, 1l);
-
+        Map<Byte, Long> fishCountPerTimer = parseInput();
         fishCountPerTimer = fishSimulation(fishCountPerTimer, 256);
 
         return String.valueOf(fishCountPerTimer.values().stream().mapToLong(Long::longValue).sum());
     }
 
-    private Map<Integer, Long> fishSimulation(Map<Integer, Long> fishCountPerTimer, int days) {
-        for (int d = 0; d < days; d++) {
-            Map<Integer, Long> newFishCountPerTimer = new HashMap<>();
-            fishCountPerTimer.forEach((timer, count) -> newFishCountPerTimer.put(timer - 1, count));
-            Long spawnThreshold = newFishCountPerTimer.remove(-1);
-            if (spawnThreshold != null) {
-                if (newFishCountPerTimer.computeIfPresent(8, (timer, count) -> count + spawnThreshold) == null)
-                    newFishCountPerTimer.put(8, spawnThreshold);
+    private Map<Byte, Long> parseInput() throws IOException, URISyntaxException {
+        Path path = Paths.get(getClass().getClassLoader().getResource("day06.txt").toURI());
+        List<String> lines = Files.readAllLines(path);
+        List<Byte> fish = Arrays.stream(lines.get(0).split(",")).map(Byte::valueOf).toList();
+        Map<Byte, Long> fishCountPerTimer = new HashMap<>();
+        for (var f : fish)
+            if (fishCountPerTimer.computeIfPresent(f, (timer, count) -> count + 1) == null)
+                fishCountPerTimer.put(f, 1l);
 
-                if (newFishCountPerTimer.computeIfPresent(6, (timer, count) -> count + spawnThreshold) == null)
-                    newFishCountPerTimer.put(6, spawnThreshold);
+        return fishCountPerTimer;
+    }
+
+    private Map<Byte, Long> fishSimulation(Map<Byte, Long> fishCountPerTimer, int days) {
+        for (int d = 0; d < days; d++) {
+            Map<Byte, Long> newFishCountPerTimer = new HashMap<>();
+            fishCountPerTimer.forEach((timer, count) -> newFishCountPerTimer.put((byte)(timer - 1), count));
+            Long spawnThreshold = newFishCountPerTimer.remove((byte)-1);
+            if (spawnThreshold != null) {
+                if (newFishCountPerTimer.computeIfPresent((byte)8, (timer, count) -> count + spawnThreshold) == null)
+                    newFishCountPerTimer.put((byte)8, spawnThreshold);
+
+                if (newFishCountPerTimer.computeIfPresent((byte)6, (timer, count) -> count + spawnThreshold) == null)
+                    newFishCountPerTimer.put((byte)6, spawnThreshold);
             }
 
             fishCountPerTimer = newFishCountPerTimer;
