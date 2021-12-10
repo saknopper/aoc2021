6344ccff55a980386e127cac90e260102e0a2a63
Sander Knopper <sander@knopper-ict.nl>
2021-12-10 20:39:01 +0100

    Day 10 - cleanups

--- src/main/java/com/github/saknopper/aoc2021/days/Day10.java
+++ src/main/java/com/github/saknopper/aoc2021/days/Day10.java
@@ -4,11 +4,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Day10 extends Day
 {
@@ -20,41 +20,40 @@ public class Day10 extends Day
     @Override
     public String getAnswerPartOne() throws Exception {
         Path path = Paths.get(getClass().getClassLoader().getResource("day10.txt").toURI());
-
-        long points = 0l;
-        for (var l : Files.readAllLines(path)) {
+        int points = Files.readAllLines(path).stream().map(l -> {
             Deque<String> openings = new ArrayDeque<>();
             for (var ch : l.toCharArray()) {
                 String token = String.valueOf(ch);
                 if (VALID_CHUNK_PAIRS.keySet().contains(token))
                     openings.push(token);
-                else if (VALID_CHUNK_PAIRS.values().contains(token)) {
+                else {
                     String openingToken = openings.pop();
                     if (!VALID_CHUNK_PAIRS.get(openingToken).equals(token)) {
-                        points += ILLEGAL_CHAR_SCORES.get(token);
-                        break;
+                        return ILLEGAL_CHAR_SCORES.get(token);
                     }
                 }
             }
-        }
+
+            return Integer.valueOf(0);
+        }).mapToInt(Integer::intValue).sum();
 
         return String.valueOf(points);
     }
 
     @Override
     public String getAnswerPartTwo() throws Exception {
-        List<Long> scores = new ArrayList<>();
-
         Path path = Paths.get(getClass().getClassLoader().getResource("day10.txt").toURI());
-        List<String> incompleteLines = Files.readAllLines(path).stream().filter(Day10::isNotCorruptedLine).toList();
-        for (var l : incompleteLines) {
+        List<Long> scores = Files.readAllLines(path).stream().map(l -> {
             Deque<String> openings = new ArrayDeque<>();
             for (var ch : l.toCharArray()) {
                 String token = String.valueOf(ch);
                 if (VALID_CHUNK_PAIRS.keySet().contains(token))
                     openings.push(token);
-                else
-                    openings.pop();
+                else {
+                    String openingToken = openings.pop();
+                    if (!VALID_CHUNK_PAIRS.get(openingToken).equals(token))
+                        return Long.valueOf(-1l);
+                }
             }
 
             long score = 0l;
@@ -64,27 +63,11 @@ public class Day10 extends Day
                 score += AUTOCOMPLETE_CHAR_SCORES.get(token);
             }
 
-            scores.add(score);
-        }
+            return Long.valueOf(score);
+        }).filter(score -> !Long.valueOf(-1l).equals(score)).collect(Collectors.toList());
 
         Collections.sort(scores);
 
         return String.valueOf(scores.get(scores.size() / 2));
-    }
-
-    private static boolean isNotCorruptedLine(String l) {
-        Deque<String> openings = new ArrayDeque<>();
-        for (var ch : l.toCharArray()) {
-            String token = String.valueOf(ch);
-            if (VALID_CHUNK_PAIRS.keySet().contains(token))
-                openings.push(token);
-            else if (VALID_CHUNK_PAIRS.values().contains(token)) {
-                String openingToken = openings.pop();
-                if (!VALID_CHUNK_PAIRS.get(openingToken).equals(token))
-                    return false;
-            }
-        }
-
-        return true;
     }
 }
