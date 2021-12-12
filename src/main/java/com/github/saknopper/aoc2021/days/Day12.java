ae738a4bd767a6fe547a8d4854e85301b7d49cbe
Sander Knopper <sander@knopper-ict.nl>
2021-12-12 12:31:29 +0100

    Day 12

---
+++ src/main/java/com/github/saknopper/aoc2021/days/Day12.java
@@ -0,0 +1,90 @@
+package com.github.saknopper.aoc2021.days;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+public class Day12 extends Day
+{
+    private static final String START_VERTEX = "start";
+    private static final String END_VERTEX = "end";
+
+    @Override
+    public String getAnswerPartOne() throws Exception {
+        Multimap<String, String> edges = parseInputToMultimap();
+        List<List<String>> paths = createValidPaths(edges, false);
+
+        return String.valueOf(paths.size());
+    }
+
+    @Override
+    public String getAnswerPartTwo() throws Exception {
+        Multimap<String, String> edges = parseInputToMultimap();
+        List<List<String>> paths = createValidPaths(edges, true);
+
+        return String.valueOf(paths.size());
+    }
+
+    private Multimap<String, String> parseInputToMultimap() throws URISyntaxException, IOException {
+        Path path = Paths.get(getClass().getClassLoader().getResource("day12.txt").toURI());
+        Multimap<String, String> edges = ArrayListMultimap.create();
+
+        Files.readAllLines(path).stream().forEach(line -> {
+            String[] splitted = line.split("-");
+            edges.put(splitted[0], splitted[1]);
+            edges.put(splitted[1], splitted[0]);
+        });
+
+        return edges;
+    }
+
+    private static List<List<String>> createValidPaths(Multimap<String, String> edges, boolean allowedToVisitOneSmallCaveTwice) {
+        List<List<String>> paths = new ArrayList<>();
+        edges.get(START_VERTEX).forEach(step -> {
+            List<String> path = new ArrayList<>(List.of(START_VERTEX));
+            paths.addAll(nextStepInPath(path, step, edges, allowedToVisitOneSmallCaveTwice));
+        });
+
+        return paths;
+    }
+
+    private static List<List<String>> nextStepInPath(List<String> path, String currentStep, Multimap<String, String> edges,
+            boolean allowedToVisitOneSmallCaveTwice) {
+        path = new ArrayList<>(path);
+        path.add(currentStep);
+        if (END_VERTEX.equals(currentStep))
+            return List.of(path);
+
+        List<List<String>> paths = new ArrayList<>();
+        List<String> nextSteps = edges.get(currentStep).stream().filter(step -> !START_VERTEX.equals(step)).toList();
+        for (var nextStep : nextSteps) {
+            if (nextStep.toLowerCase().equals(nextStep) && path.contains(nextStep)) {
+                if (allowedToVisitOneSmallCaveTwice) {
+                    if (containsSmallCaveVisitedMultipleTimes(path))
+                        continue;
+                } else {
+                    continue;
+                }
+            }
+
+            paths.addAll(nextStepInPath(path, nextStep, edges, allowedToVisitOneSmallCaveTwice));
+        }
+
+        return paths;
+    }
+
+    private static boolean containsSmallCaveVisitedMultipleTimes(List<String> path) {
+        for (var pos : path)
+            if (pos.toLowerCase().equals(pos) && path.indexOf(pos) != path.lastIndexOf(pos))
+                return true;
+
+        return false;
+    }
+}
